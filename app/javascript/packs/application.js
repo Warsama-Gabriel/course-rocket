235e57204fe73e8b5261279c408ef292bc99a846
Warsama Gabriel <warsamagabriel@Warsamas-MacBook-Pro-2.local>
2023-09-19 19:49:24 -0700

    Configured webpacker to work with boostrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,13 +2,27 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import "bootstrap/dist/css/bootstrap.css";
+// Import Bootstrap JavaScript
+import "bootstrap/dist/js/bootstrap";
+// Import Popper.js
+import "popper.js/dist/popper";
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+// Import jQuery
+import $ from "jquery";
 
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
+// Initialize Bootstrap tooltips and popovers
+$(document).on("turbolinks:load", function () {
+  $('[data-toggle="tooltip"]').tooltip();
+  $('[data-toggle="popover"]').popover();
+});
+
+//
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
